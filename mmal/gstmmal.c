3e98a3991e76e53a7dd3a5ef23f7e2ffee7fcca8
Krzysztof Konopko <kris@youview.com>
2015-12-15 16:30:51 +0100

    Add "empty" gst-mmal project
    This commit adds GStreamer boilerplate files to make a fully-fledged
    GStreamer project.  No functionality is provided yet.

---
+++ mmal/gstmmal.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015, YouView TV Ltd.
+ *   Author: Krzysztof Konopko <kris@youview.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+
+#include <gst/gst.h>
+
+static gboolean
+plugin_init (GstPlugin * plugin)
+{
+  return TRUE;
+}
+
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    mmal,
+    "GStreamer MMAL Plug-ins",
+    plugin_init,
+    PACKAGE_VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
